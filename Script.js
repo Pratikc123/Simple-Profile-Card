61190fe1552e198dcffd6027b64759fd73e7d574
Pratik Chaudhari <88817003+Pratikc123@users.noreply.github.com>
2025-04-01 11:13:40 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,4 @@
+const fontAwesomeScript = document.createElement("script");
+fontAwesomeScript.src = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js";
+fontAwesomeScript.crossOrigin = "anonymous";
+document.head.appendChild(fontAwesomeScript);
